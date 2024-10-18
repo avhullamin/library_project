3caaa2d97959ebe8a9f71dd4aceebf71e16d9c44
OMEGA-5656 <abhisheksaini642794@gmail.com>
2024-10-18 15:46:37 +0530

    add login class +a background

---
+++ app/src/main/java/com/example/unisaplibraryapp/LoginPage.java
@@ -0,0 +1,20 @@
+package com.example.unisaplibraryapp;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        //full screen mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/unisaplibraryapp/MainActivity.java
+++ app/src/main/java/com/example/unisaplibraryapp/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.unisaplibraryapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, LoginPage.class);
+        startActivity(intent);
+        finish(); // Finish MainActivity so that the user can't return to it
     }
 }
