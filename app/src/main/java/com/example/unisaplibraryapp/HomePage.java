1f175395cdb0d61fe70cb03ea7921241d114952b
OMEGA-5656 <abhisheksaini642794@gmail.com>
2025-03-08 14:27:16 +0530

    added test button

--- app/src/main/java/com/example/unisaplibraryapp/HomePage.java
+++ app/src/main/java/com/example/unisaplibraryapp/HomePage.java
@@ -1,13 +1,24 @@
 package com.example.unisaplibraryapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePage extends AppCompatActivity {
-
+    TextView homepage_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage); // Use the new layout file
+
+        homepage_text = (android.widget.TextView) findViewById(R.id.homepage_text);
+    }
+    @SuppressLint("SetTextI18n")
+    public void changeHeader(View view){
+        homepage_text.setText("testButton");
+        System.out.println("testButton clicked!");
     }
 }
